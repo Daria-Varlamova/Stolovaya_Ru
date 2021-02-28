cc0b86ec39ab18bd803263190f984bd7d59d8fc6
Daria-Varlamova <smile_leo@mail.ru>
2021-02-28 18:37:01 +0300

    init

--- src/scripts/burgerMenu.js
+++ src/scripts/burgerMenu.js
@@ -2,9 +2,18 @@
 let body = document.querySelector("body");
 let burger = body.querySelector(".burger-menu");
 let navList = body.querySelector(".menu-nav__list");
+let menuNavItem = body.querySelectorAll('.menu-nav__item');
+
+console.log(menuNavItem)
 
 function toggleMenu () {
    body.classList.toggle("open-menu");
 }
 
 burger.addEventListener("click", toggleMenu);
+
+for (let i = 0; i < menuNavItem.length; i ++) {
+   menuNavItem[i].addEventListener('click' , function  (){
+      body.classList.remove("open-menu");
+   })
+}
